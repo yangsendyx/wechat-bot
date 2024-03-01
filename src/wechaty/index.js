de7cd20f208f26615b73f2004397a8fee72da80b
yangsen <sen@neradigm.com>
2024-03-01 11:53:07 +0800

    add docker ci

--- src/wechaty/index.js
+++ src/wechaty/index.js
@@ -3,37 +3,37 @@ import qrTerminal from 'qrcode-terminal'
 import { defaultMessage, shardingMessage } from './sendMessage.js'
 // 扫码
 function onScan(qrcode, status) {
-  if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
-    // 在控制台显示二维码
-    qrTerminal.generate(qrcode, { small: true })
-    const qrcodeImageUrl = ['https://api.qrserver.com/v1/create-qr-code/?data=', encodeURIComponent(qrcode)].join('')
-    console.log('onScan:', qrcodeImageUrl, ScanStatus[status], status)
-  } else {
-    log.info('onScan: %s(%s)', ScanStatus[status], status)
-  }
+    if (status === ScanStatus.Waiting || status === ScanStatus.Timeout) {
+        // 在控制台显示二维码
+        qrTerminal.generate(qrcode, { small: true })
+        const qrcodeImageUrl = ['https://api.qrserver.com/v1/create-qr-code/?data=', encodeURIComponent(qrcode)].join('')
+        console.log('onScan:', qrcodeImageUrl, ScanStatus[status], status)
+    } else {
+        log.info('onScan: %s(%s)', ScanStatus[status], status)
+    }
 }
 
 // 登录
 function onLogin(user) {
-  console.log(`${user} has logged in`)
-  const date = new Date()
-  console.log(`Current time:${date}`)
-  console.log(`Automatic robot chat mode has been activated`)
+    console.log(`${user} has logged in`)
+    const date = new Date()
+    console.log(`Current time:${date}`)
+    console.log(`Automatic robot chat mode has been activated`)
 }
 
 // 登出
 function onLogout(user) {
-  console.log(`${user} has logged out`)
+    console.log(`${user} has logged out`)
 }
 
 // 收到好友请求
 async function onFriendShip(friendship) {
-  const frienddShipRe = /chatgpt|chat/
-  if (friendship.type() === 2) {
-    if (frienddShipRe.test(friendship.hello())) {
-      await friendship.accept()
+    const frienddShipRe = /chatgpt|chat/
+    if (friendship.type() === 2) {
+        if (frienddShipRe.test(friendship.hello())) {
+            await friendship.accept()
+        }
     }
-  }
 }
 
 /**
@@ -43,22 +43,22 @@ async function onFriendShip(friendship) {
  * @returns {Promise<void>}
  */
 async function onMessage(msg) {
-  // 默认消息回复
-  await defaultMessage(msg, bot)
-  // 消息分片
-  // await shardingMessage(msg,bot)
+    // 默认消息回复
+    await defaultMessage(msg, bot)
+    // 消息分片
+    // await shardingMessage(msg,bot)
 }
 
 // 初始化机器人
 const CHROME_BIN = process.env.CHROME_BIN ? { endpoint: process.env.CHROME_BIN } : {}
 export const bot = WechatyBuilder.build({
-  name: 'WechatEveryDay',
-  puppet: 'wechaty-puppet-wechat4u', // 如果有token，记得更换对应的puppet
-  // puppet: 'wechaty-puppet-wechat', // 如果 wechaty-puppet-wechat 存在问题，也可以尝试使用上面的 wechaty-puppet-wechat4u ，记得安装 wechaty-puppet-wechat4u
-  puppetOptions: {
-    uos: true,
-    ...CHROME_BIN
-  },
+    name: 'WechatEveryDay',
+    puppet: 'wechaty-puppet-wechat4u', // 如果有token，记得更换对应的puppet
+    // puppet: 'wechaty-puppet-wechat', // 如果 wechaty-puppet-wechat 存在问题，也可以尝试使用上面的 wechaty-puppet-wechat4u ，记得安装 wechaty-puppet-wechat4u
+    puppetOptions: {
+        uos: true,
+        ...CHROME_BIN
+    },
 })
 
 // 扫码
@@ -74,6 +74,6 @@ bot.on('friendship', onFriendShip)
 
 // 启动微信机器人
 bot
-  .start()
-  .then(() => console.log('Start to log in wechat...'))
-  .catch((e) => console.error(e))
+    .start()
+    .then(() => console.log('Start to log in wechat...'))
+    .catch((e) => console.error(e))
